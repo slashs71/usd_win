e9bdb5c37c8fc87d2d4f0642d00c34509090239b
S M <slashs@mail.ru>
2021-06-15 23:13:49 +0300

    Добавлены файлы проекта

---
+++ UART_rcv/UART_rcv/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace UART_rcv {
+    partial class frmUSDoppler {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.cmbPortName = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tmrCheckComPorts = new System.Windows.Forms.Timer(this.components);
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnSigGenStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UpDown_Amax = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDown_Amax)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpenPort);
+            this.groupBox1.Controls.Add(this.cmbPortName);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(146, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "СОМ-порт";
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpenPort.Location = new System.Drawing.Point(12, 58);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(121, 23);
+            this.btnOpenPort.TabIndex = 1;
+            this.btnOpenPort.Text = "Открыть порт";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // cmbPortName
+            // 
+            this.cmbPortName.FormattingEnabled = true;
+            this.cmbPortName.Location = new System.Drawing.Point(12, 19);
+            this.cmbPortName.Name = "cmbPortName";
+            this.cmbPortName.Size = new System.Drawing.Size(121, 21);
+            this.cmbPortName.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(189, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(833, 224);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // tmrCheckComPorts
+            // 
+            this.tmrCheckComPorts.Enabled = true;
+            this.tmrCheckComPorts.Interval = 500;
+            this.tmrCheckComPorts.Tick += new System.EventHandler(this.tmrCheckComPorts_Tick);
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chart2.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            this.chart2.Location = new System.Drawing.Point(189, 251);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(833, 224);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart1";
+            // 
+            // btnSigGenStart
+            // 
+            this.btnSigGenStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSigGenStart.Location = new System.Drawing.Point(12, 102);
+            this.btnSigGenStart.Name = "btnSigGenStart";
+            this.btnSigGenStart.Size = new System.Drawing.Size(121, 23);
+            this.btnSigGenStart.TabIndex = 1;
+            this.btnSigGenStart.Text = "Сигнал";
+            this.btnSigGenStart.UseVisualStyleBackColor = true;
+            this.btnSigGenStart.Visible = false;
+            this.btnSigGenStart.Click += new System.EventHandler(this.btnSigGenStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Амплитуда FFT";
+            // 
+            // UpDown_Amax
+            // 
+            this.UpDown_Amax.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.UpDown_Amax.Location = new System.Drawing.Point(99, 173);
+            this.UpDown_Amax.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.UpDown_Amax.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.UpDown_Amax.Name = "UpDown_Amax";
+            this.UpDown_Amax.Size = new System.Drawing.Size(70, 20);
+            this.UpDown_Amax.TabIndex = 4;
+            this.UpDown_Amax.Value = new decimal(new int[] {
+            30000,
+            0,
+            0,
+            0});
+            this.UpDown_Amax.ValueChanged += new System.EventHandler(this.UpDown_Amax_ValueChanged);
+            // 
+            // frmUSDoppler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1034, 542);
+            this.Controls.Add(this.UpDown_Amax);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSigGenStart);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmUSDoppler";
+            this.Text = "USD";
+            this.Load += new System.EventHandler(this.frmUSDoppler_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDown_Amax)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox cmbPortName;
+        private System.Windows.Forms.Timer tmrCheckComPorts;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button btnSigGenStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown UpDown_Amax;
+    }
+}
+
---
+++ UART_rcv/UART_rcv/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace UART_rcv
+{
+    public partial class frmUSDoppler : Form
+    {
+        #region Local Variables
+
+        // The main control for communicating through the RS-232 port
+        private SerialPort comport = new SerialPort();
+        private SerialPort comport_sig = new SerialPort();
+
+
+        private List<int> buf = new List<int>();
+        #endregion
+
+        public frmUSDoppler()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrCheckComPorts_Tick(object sender, EventArgs e)
+        {
+            // checks to see if COM ports have been added or removed
+            // since it is quite common now with USB-to-Serial adapters
+            RefreshComPortList();
+        }
+
+        private void RefreshComPortList()
+        {
+            // Determain if the list of com port names has changed since last checked
+            string selected = RefreshComPortList(cmbPortName.Items.Cast<string>(), cmbPortName.SelectedItem as string, comport.IsOpen);
+
+            // If there was an update, then update the control showing the user the list of port names
+            if (!String.IsNullOrEmpty(selected))
+            {
+                cmbPortName.Items.Clear();
+                cmbPortName.Items.AddRange(OrderedPortNames());
+                cmbPortName.SelectedItem = selected;
+            }
+        }
+
+        private string[] OrderedPortNames()
+        {
+            // Just a placeholder for a successful parsing of a string to an integer
+            int num;
+
+            // Order the serial port names in numberic order (if possible)
+            return SerialPort.GetPortNames().OrderBy(a => a.Length > 3 && int.TryParse(a.Substring(3), out num) ? num : 0).ToArray();
+        }
+
+        private string RefreshComPortList(IEnumerable<string> PreviousPortNames, string CurrentSelection, bool PortOpen)
+        {
+            // Create a new return report to populate
+            string selected = null;
+
+            // Retrieve the list of ports currently mounted by the operating system (sorted by name)
+            string[] ports = SerialPort.GetPortNames();
+
+            // First determain if there was a change (any additions or removals)
+            bool updated = PreviousPortNames.Except(ports).Count() > 0 || ports.Except(PreviousPortNames).Count() > 0;
+
+            // If there was a change, then select an appropriate default port
+            if (updated)
+            {
+                // Use the correctly ordered set of port names
+                ports = OrderedPortNames();
+
+                // Find newest port if one or more were added
+                string newest = SerialPort.GetPortNames().Except(PreviousPortNames).OrderBy(a => a).LastOrDefault();
+
+                // If the port was already open... (see logic notes and reasoning in Notes.txt)
+                if (PortOpen)
+                {
+                    if (ports.Contains(CurrentSelection)) selected = CurrentSelection;
+                    else if (!String.IsNullOrEmpty(newest)) selected = newest;
+                    else selected = ports.LastOrDefault();
+                }
+                else
+                {
+                    if (!String.IsNullOrEmpty(newest)) selected = newest;
+                    else if (ports.Contains(CurrentSelection)) selected = CurrentSelection;
+                    else selected = ports.LastOrDefault();
+                }
+            }
+
+            // If there was a change to the port list, return the recommended default selection
+            return selected;
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            bool error = false;
+
+            // If the port is open, close it.
+            if (comport.IsOpen)
+            {
+                try
+                {
+                    String str = comport.ReadExisting();
+                    comport.DiscardInBuffer();                    
+                    comport.Close();
+                    comport.DiscardInBuffer();
+
+                }
+                catch (Exception)
+                {
+                }
+            }
+            else
+            {
+                // Set the port's settings
+
+                comport.DtrEnable = false;
+                comport.RtsEnable = false;
+
+
+                comport.BaudRate = 921600;
+                comport.DataBits = (int)8;
+                comport.StopBits = StopBits.One;
+                comport.Parity = Parity.None;
+                comport.PortName = cmbPortName.Text;
+                comport.ReadTimeout = 500;
+                comport.WriteTimeout = 500;
+                //comport.ReceivedBytesThreshold = 2;
+
+
+                try
+                {
+                    // Open the port
+                    comport.Open();
+                }
+                catch (UnauthorizedAccessException) { error = true; }
+                catch (IOException) { error = true; }
+                catch (ArgumentException) { error = true; }
+
+                if (error) MessageBox.Show(this, "Could not open the COM port.  Most likely it is already in use, has been removed, or is unavailable.", "COM Port Unavalible", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+
+            // Change the state of the form's controls
+            //EnableControls();
+            if (comport.IsOpen) btnOpenPort.Text = "Закрыть порт";
+            else btnOpenPort.Text = "Открыть порт";
+
+            // open_comsig();
+        }
+
+        private void frmUSDoppler_Load(object sender, EventArgs e)
+        {
+            comport.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+        }
+
+        private void open_comsig()
+        {
+            bool error = false;
+
+            // If the port is open, close it.
+            if (comport_sig.IsOpen)
+            {
+                comport_sig.Close();
+            }
+            else
+            {
+                // Set the port's settings
+
+                comport_sig.DtrEnable = false;
+                comport.RtsEnable = false;
+
+
+                comport_sig.BaudRate = 921600;
+                comport_sig.DataBits = (int)8;
+                comport_sig.StopBits = StopBits.One;
+                comport_sig.Parity = Parity.None;
+                comport_sig.PortName = "com1";
+                comport_sig.ReadTimeout = 500;
+                comport_sig.WriteTimeout = 500;
+                //comport.ReceivedBytesThreshold = 2;
+
+
+                try
+                {
+                    // Open the port
+                    comport_sig.Open();
+                }
+                catch (UnauthorizedAccessException) { error = true; }
+                catch (IOException) { error = true; }
+                catch (ArgumentException) { error = true; }
+
+                if (error) MessageBox.Show(this, "Could not open the COM port SIGNAL GENERATOR.  Most likely it is already in use, has been removed, or is unavailable.", "COM Port Unavalible", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!comport.IsOpen) return;
+
+            int NN = 512;
+            int bytes;
+
+            bytes = comport.BytesToRead;
+            while ((bytes > 0) && comport.IsOpen)
+            {
+                try
+                {
+
+
+                    buf.Add(comport.ReadByte());
+                    bytes = comport.BytesToRead;
+                    //comport.DiscardInBuffer();
+                    if (buf.Count >= NN)
+                    {
+                        if (comport.IsOpen) this.Invoke(new EventHandler(delegate { updateGraph(buf.ToArray()); }));
+                        buf.Clear();
+                    }
+                }
+                catch
+                {
+
+                }
+
+            }
+        }
+
+        private void updateGraph(int[] v)
+        {
+            
+            int N_FFT = 512;
+            int[] vv = new int[v.Length];
+
+            alglib.complex[] f1;
+            double[] d = new double[v.Length];
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] > 128)
+                {
+                    vv[i] = v[i] - 256;
+                }
+                else
+                {
+                    vv[i] = v[i];
+                }
+            }
+
+            //this.Text = buf.Count.ToString();
+            this.Text = v.Length.ToString();
+            chart1.Series[0].Points.Clear();
+            chart1.ChartAreas[0].AxisY.Minimum = -200;
+            chart1.ChartAreas[0].AxisY.Maximum = 200;
+            foreach (var b in vv)
+            {
+
+                chart1.Series[0].Points.AddY(b);
+            }
+
+            for (int i = 0; i < vv.Length; i++)
+            {
+                d[i] = Convert.ToDouble(vv[i]);
+            }
+            alglib.fftr1d(d, N_FFT, out f1);
+
+            chart2.Series[0].Points.Clear();
+            chart2.ChartAreas[0].AxisY.Maximum = Convert.ToDouble(UpDown_Amax.Value);
+            for (int i = 0; i < N_FFT / 2; i++)
+            {
+                chart2.Series[0].Points.AddY(alglib.math.abscomplex(f1[i]));
+            }
+
+        }
+
+        private void btnSigGenStart_Click(object sender, EventArgs e)
+        {
+            byte[] wrbuf = new byte[512];
+            int N = wrbuf.Length;
+            byte d = 0;
+
+            double Fs = 20000;
+            double Ts = 1 / Fs;
+            double F = 1000;
+            double arg = 0;
+
+            for (int i = 0; i < 511; i++)
+            {
+                arg = 2 * Math.PI * F * Ts * i;
+                d = Convert.ToByte((Math.Round(Math.Sin(arg) * 120) + 120 + alglib.math.randominteger(15)) % 255);
+
+                wrbuf[i] = d;
+            }
+
+            if (comport_sig.IsOpen)
+            {
+                comport_sig.Write(wrbuf, 0, N);
+            }
+        }
+
+        private void UpDown_Amax_ValueChanged(object sender, EventArgs e)
+        {
+            chart2.ChartAreas[0].AxisY.Maximum = Convert.ToDouble(UpDown_Amax.Value);
+        }
+    }
+}
